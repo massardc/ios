0178cc8d6e376520cab180d41725a6a0a6f0698e
Clement M <massardc@users.noreply.github.com>
2017-08-10 22:53:07 +0200

    Swoosh 07, added 2 view controllers

---
+++ Swoosh/Swoosh/Controller/WelcomeVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Copyright © 2017 ClementM. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
+
+
+}
+
