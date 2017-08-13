788c445d226d38af3c8c6c6bc2a483de7e17b3af
Clement M <massardc@users.noreply.github.com>
2017-08-13 19:27:07 +0200

    Segues and passing data between VCs

---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Copyright © 2017 ClementM. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+}
